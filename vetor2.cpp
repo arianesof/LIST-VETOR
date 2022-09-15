efdd207967d9b4058d38b7e855353293bd53595f
Ariane Sousa <111510431+arianesof@users.noreply.github.com>
2022-09-15 11:04:29 -0300

    Add files via upload

---
+++ vetor2.cpp
@@ -0,0 +1,23 @@
+#include<stdio.h>
+int main(){
+	int idade[3],i;
+	int numeroAlunos;
+	printf("Infofrme a quatidade de alunos");
+	scanf("d", &numeroAlunos);
+	double notas [numeroAlunos]
+	int i;
+	
+	
+	for (i=0;i<numeroAlunos;i++){
+		printf("Digite a nota: ");
+		scanf("%lf", &nota[i]);
+
+		
+	}
+		for (i=0;i<numeroAlunos;i++){
+			printf("%.1lf\t", nota[i]);		
+	}
+	 	printf("%d", idade[3]);
+	
+	}
+	
